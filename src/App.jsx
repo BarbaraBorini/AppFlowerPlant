ffdd8764ae5cc252e2e4d105fe6e4fff59c41bf0
BarbaraBorini <eaa25babo@students.eaaa.dk>
2026-03-03 04:39:23 +0100

    basename change

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
-import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout'
 import Home from './views/Home.jsx'
 import MyPlants from './views/MyPlants.jsx'
 import About from './views/About.jsx'
 
-// The header and footer are always visible, only the <Routes> area changes when navigation happens.
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: 'my-plants',
+          element: <MyPlants />,
+        },
+        {
+          path: 'about',
+          element: <About />,
+        },
+      ],
+    },
+  ],
+  {
 
-function App() {
-  return (
-    <div className="app">
-      <Header />
-      <main className="app-main">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/my-plants" element={<MyPlants />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
-  )
+    basename: "/AppFlowerPlant",
+  }
+)
+
+export default function App() {
+  return <RouterProvider router={router} />
 }
-
-export default App
